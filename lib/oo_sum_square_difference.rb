f825e03bca31692e35d9c903a08959bbf32500f6
bmolina-nyc <bruce.d.molina@gmail.com>
2016-03-01 20:32:34 -0500

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,26 @@
 # Implement your object-oriented solution here!
+
+
+class SumSquareDifference
+
+  attr_reader :value 
+
+  def initialize(number)
+    @value = number 
+  end
+
+  def sum_of_squares
+    1.upto(value).to_a.map!{|el| el * el}.inject{ |x,y| x + y }
+  end
+
+  def square_of_sum
+    result = 1.upto(value).to_a.inject{ |x,y| x + y }
+    result * result 
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+
+
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,17 @@
 # Implement your procedural solution here!
+
+def square_sum(number)
+  array_sum = 1.upto(number).to_a.inject { |x,y| x + y }
+  array_sum * array_sum
+end
+
+def sum_square(number)
+  array = 1.upto(number).to_a.map! { |num| num * num }
+  array.inject { |x,y| x + y }
+end
+
+
+
+def sum_square_difference(number)
+  square_sum(number) - sum_square(number)
+end
